6e79e5eb4085c7cb9a7ce888aa19dbd08c5451c5
itamar sandovski <itamarsando@gmail.com>
2018-08-12 18:26:29 +0300

    four

--- main.js
+++ main.js
@@ -19,3 +19,37 @@ reduce(items, callback, intialVal);{
 
 // exercise 3
 
+class EventEmitter {
+    constructor(){
+        this.event = {};
+    }
+    on(eventName, callback){
+        if (typeof is.events[eventName] !== 'object') {
+            this.events[eventName] = [];
+        }
+
+        this.events[eventName].push(callback);
+    }
+
+    emit(eventName, payload) {
+        if (this.events[eventName]){
+            this.events[eventName].forEach((callback) => {
+                callback(payload);
+            });
+            } else {
+            console.log("this event doesn't excies");
+        }
+    }
+
+    off(eventName,callback){
+        var index;
+
+        if (typeof this.events[eventName] === 'object'){
+            index = this.events[eventName].indexOf(callback);
+
+            if (ndex > -1){
+                this.events[eventName].aplice(index, 1);
+            }
+        }
+    }
+}
