4fe64887feb7a2432ec1cc0b8cefd91375a2ea3e
itamar sandovski <itamarsando@gmail.com>
2018-08-12 18:10:38 +0300

    tree

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+//exercise 1
+
 const items = [4,8,5,9];
 const mult = items.reduce(function (a,b,c,d) {return a+b+c+d;}); //=>26
 
+
+// exercise 2
 reduce(items, callback, intialVal);{
     var accumulator = (intialVal === undefined) ? undefined : intialVal;
     for (var i = 0; i < items.length; i++){
@@ -11,3 +15,7 @@ reduce(items, callback, intialVal);{
     }
     return accumulator;
 };
+
+
+// exercise 3
+
