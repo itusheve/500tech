991c9d95d8eeca0812a76426d2ced5b8b8f8187e
itamar sandovski <itamarsando@gmail.com>
2018-08-12 18:08:11 +0300

    two

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const items = [4,8,5,9];
 const mult = items.reduce(function (a,b,c,d) {return a+b+c+d;}); //=>26
-console.log(mult);
+
+reduce(items, callback, intialVal);{
+    var accumulator = (intialVal === undefined) ? undefined : intialVal;
+    for (var i = 0; i < items.length; i++){
+        if(accumulatori== undefined)
+            accumulator = callback.call(undefined, accumulator, items[i], i, items);
+        else
+            accumulator = items[i];
+    }
+    return accumulator;
+};
